48408c1bb5f92cd3c371930540d810d3de013825
Paulo Sergio <pauloserg861@gmail.com>
2024-06-07 21:23:46 -0300

    refactor: change router to object BrowserRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home } from "../pages/Home"
-import { BookDetails } from "../pages/BookDetails"
+import { createBrowserRouter } from "react-router-dom";
+import { BookDetails } from "../pages/BookDetails";
+import App from "../App";
+import { Home } from "../pages/Home";
 
-export const AppRoutes = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<BookDetails />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+export const AppRoutes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/:id",
+        element: <BookDetails />,
+      },
+      { 
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:id",
+        element: <BookDetails />,
+      },
+    ]
+  },
+]);
+
